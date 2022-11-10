05256720a44806c23a964840826f444f5e16f4be
sweet202020 <dolcealessio1@gmail.com>
2022-11-10 16:16:05 +0100

    add siteHeader with navbar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <script>
+import siteHeader from "./components/siteHeader.vue";
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    siteHeader,
+  }
 }
 
 </script>
 
 
 <template>
-  <header id="site_header">
-    <div class="d-flex">
-      <div class="logo">
-        <img src="./assets/img/dc-logo.png" alt="">
-      </div>
-      <nav class="nav justify-content-center  ">
-        <a class="nav-link active" href="#" aria-current="page">Active link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link disabled" href="#">Disabled link</a>
-      </nav>
-
-    </div>
-  </header>
+  <siteHeader />
   <!-- /#site_header -->
   <main id="site_main"></main>
   <!-- /#site_main -->
@@ -30,12 +22,5 @@ export default {
 
 
 <style lang="scss">
-nav {
-  background-color: red;
 
-  a {
-    text-decoration: none;
-    color: white;
-  }
-}
 </style>
---
+++ src/components/siteHeader.vue
@@ -0,0 +1,104 @@
+
+
+
+<script>
+export default {
+    name: 'siteHeader',
+    data() {
+        return {
+            menu:
+                [
+                    {
+                        name: 'Characters',
+                        link: '#'
+                    },
+                    {
+                        name: 'Comics',
+                        link: '#'
+                    },
+                    {
+                        name: 'Movies',
+                        link: '#'
+                    },
+                    {
+                        name: 'Tv',
+                        link: '#'
+                    },
+                    {
+                        name: 'Games',
+                        link: '#'
+                    },
+                    {
+                        name: 'Collectibles',
+                        link: '#'
+                    },
+                    {
+                        name: 'Videos',
+                        link: '#'
+                    },
+                    {
+                        name: 'Fans',
+                        link: '#'
+                    },
+                    {
+                        name: 'News',
+                        link: '#'
+                    },
+                    {
+                        name: 'Shop',
+                        link: '#'
+                    },
+                ]
+        }
+    }
+}
+</script>
+
+
+
+<template>
+    <header id="site_header">
+        <div class="container">
+            <div class="d-flex justify-content-around">
+                <div class="logo">
+                    <img src="../assets/img/dc-logo.png" alt="">
+                </div>
+                <nav class="nav justify-content-center">
+                    <a v-for="item in menu" :href="item.link" class="text-uppercase"
+                        :class="item.name === 'Comics' ? 'active' : ''">{{
+        item.name
+                        }}</a>
+                </nav>
+            </div>
+        </div>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+.logo {
+    margin-top: 0.5rem;
+
+    img {
+        width: 80px;
+
+    }
+}
+
+nav {
+
+
+    a {
+        text-decoration: none;
+        color: black;
+        padding: 1rem;
+        margin-top: 1rem;
+
+    }
+
+    .active {
+        text-decoration: underline;
+        color: blue;
+    }
+}
+</style>
